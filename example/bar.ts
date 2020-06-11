5e05d4c6bd818b8030805ed66bc744dc09847a72
Steffen <yellowgarbagebag@4s1.de>
2020-06-11 10:54:15 +0200

    First commit

---
+++ example/bar.ts
@@ -0,0 +1,7 @@
+const foo: string = "Hallo Welt"
+const bar: string = `This is a very long line. ${foo} This is a very long line. This is a very long line. This is a very long line. This is a very long line.`
+console.log(foo, bar)
+
+interface Fjklsdf {
+  name: string
+}
---
+++ example/bar2.ts
@@ -0,0 +1,11 @@
+export abstract class Bar2 {
+  public static fjaskdlf(): void {
+    console.log("")
+  }
+
+  public abstract doNot(): void
+
+  public doIt(): void {
+    console.log("")
+  }
+}
