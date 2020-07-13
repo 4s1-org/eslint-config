45942bdb2dbc94a22466527c752729faef51dc76
Steffen <yellowgarbagebag@4s1.de>
2020-07-13 09:45:23 +0200

    use single quotes

--- example/bar.ts
+++ example/bar.ts
@@ -1,4 +1,4 @@
-const foo = "Hallo Welt"
+const foo = 'Hallo Welt'
 const bar = `This is a very long line. ${foo} This is a very long line. This is a very long line. This is a very long line. This is a very long line.`
 console.log(foo, bar)
 
